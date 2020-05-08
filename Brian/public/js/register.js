2e2839e792e023315f2fee45f2ad2dd7df5287a4
Brian <briannli27@gmail.com>
2020-05-07 21:58:06 -0700

    Includes all files that make the front web-page run except for the following config files: database and passport.

---
+++ Brian/public/js/register.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('input[type="checkbox"]').click(function() {
+        if ($(this).is(":checked")) {
+            $('#password').attr('type', 'text');
+        } else if ($(this).is(":not(:checked)")) {
+            $('#password').attr('type', 'password');
+        }
+    });
+});
