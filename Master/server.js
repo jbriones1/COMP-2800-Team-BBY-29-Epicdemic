6b8f50675d25d1f67ab5b74a0d23cd5b785d1590
Brian <briannli27@gmail.com>
2020-05-22 01:00:18 -0700

    Comments for server.js

--- Master/server.js
+++ Master/server.js
@@ -5,42 +5,44 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var path = require('path');
 var favicon = require('serve-favicon');
+var flash = require('connect-flash');
 
 var app = express();
 var port = process.env.PORT || 5000;
 
+/* passport.js */
 var passport = require('passport');
-var flash = require('connect-flash');
-
 require('./config/passport.js')(passport);
-var leaderboard = require('./app/leaderboard');
 
-app.use(express.static(__dirname + '/public'));
+/* ejs */
+app.set('view engine', 'ejs');
 
-
+/* app.use */
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-app.set('view engine', 'ejs');
-
-
+// serve static files
+app.use(express.static(__dirname + '/public'));
+// Session
 app.use(session({
     secret: 'justasecret',
     resave: true,
     saveUninitialized: true
 }));
-
+// Use passport.js
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+// Routes
 require('./app/routes.js')(app, passport);
 
+// Favicon
 app.use(favicon(__dirname + '/public/images/favicon/img/favicon.ico'));
 
+// Listen
 app.listen(port, function() {
     console.log("App listening on port: " + port);
 });
